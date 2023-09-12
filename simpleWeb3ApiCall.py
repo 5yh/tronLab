c3f64a8db0b558fcdce5d011065030d186b028bd
syh <13951968938@126.com>
2023-09-12 13:26:07 +0800

    修改id

--- simpleWeb3ApiCall.py
+++ simpleWeb3ApiCall.py
@@ -16,7 +16,7 @@ import argparse
 # 简单web3 api调用测试，用来提取单个交易数据
 w3 = Web3(Web3.HTTPProvider('http://localhost:50545/jsonrpc'))
 print("trc10")
-hash1='0x6c2aed1b8d1c814e8d0cf6e93d940572455ac0d327275bdb54d1ec629e26a59c'
+hash1='0x3354c1800c602996c227b6b10e462d26cce35e11f7df6d950b434417c4e68a13'
 content = w3.eth.get_transaction_receipt(hash1)
 temp = w3.eth.get_transaction(hash1)
 print(content)
